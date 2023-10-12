5d815eed8bff6895bb947e701a4d8cd5cfb17c70
Rahul-Bisht-123 <rbrahulbisht2000@gmail.com>
2023-10-12 16:52:32 +0530

    first commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+const Navigation = () => {
+  return (
+      <nav className="container">
+        <div className="logo">
+          <img src="./images/brand_logo.png" alt="nike_logo" />
+        </div>
+
+        <ul>
+          <li href='#'>MENU</li>
+          <li href='#'>LOACTION</li>
+          <li href='#'>ABOUT</li>
+          <li href='#'>CONTACT</li>
+
+        </ul>
+
+        <button>Login</button>
+      </nav>
+  )
+}
+
+export default Navigation
